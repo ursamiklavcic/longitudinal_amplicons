8e7fd1b4547fe4d046aea8b7e636d6458b003d89
Urša Miklavčič <ursmik1@nlzoh.si@vlmbngs2.nlzoh.si>
2024-05-27 10:23:06 +0000

    add libraries

--- code/5_dissimilarity.R
+++ code/5_dissimilarity.R
@@ -9,6 +9,19 @@
 # rel_abund_list: contains tables for each individual, where
 # rows are samples, column 1: sampling day, following columns counts of each OTU
 
+# Libraries 
+library(cli, lib.loc = "/home/nlzoh.si/ursmik1/R/x86_64-pc-linux-gnu-library/4.1")
+library(rlang, lib.loc = "/home/nlzoh.si/ursmik1/R/x86_64-pc-linux-gnu-library/4.1")
+library(tidyverse)
+library(vegan)
+library(lubridate)
+library(ape)
+library(ggrepel)
+library(ggpubr)
+
+set.seed(96)
+theme_set(theme_bw())
+
 # Import metadata
 metadata = as_tibble(read.csv('data/metadata.csv', sep=';')) %>%
   mutate(date=dmy(date))
@@ -33,39 +46,36 @@ otutabEM_allabund = as.data.frame(otutabEM) %>%
 
 otutabM_allabund = filter(otutabEM_allabund, substr(Group, 1, 1) == 'M')
 
-tab = otutabM_allabund %>%
+tab1 = otutabM_allabund %>%
   select(Group, name, absabund) %>%
   pivot_wider(names_from = 'name', values_from = 'absabund') %>%
   left_join(select(metadata, Group, day, person)) %>%
   column_to_rownames('Group') 
 
-# translation as best as I understand, 
-# computephi_i <- function(n1, n2) {    
-#     n1 <- equalize(n1, n2)  
-#     n2 <- equalize(n2, n1)  # naredi absolute abundances
-#     d <- n1 - n2 #izračuna d ki je reazlika med obema 
-#     s <- n1 + n2  # izračuna s ki je sešetevek med obema OTUjema 
-#     d <- d[-length(d)]  # odšeteje tiste ki jih ni v obeh točakh  oz unassigned OTUs, ki jih jaz nimam 
-#     s <- s[-length(s)]  # leave out unassigned
-#     phi <- (d^2 - s) / (s^2 - s) # izračuna phi vrednost 
-#     
-#     return(phi) 
-# }   
+# move columns day and person the the frot of the table 
+tab2 = tab1[, c(9591, 9590, 1:9589)]
+# Split data by individual
+tab = split(tab2, tab2$person)
 
+for (i in 1:9) {
+  tab[[i]] = select(tab[[i]], -person)
+}
 
-computephi_i <- function(n1, n2) {  # funkcija vazme vse OTUje točke t in t+1
-  n1 <- equalize(n1, n2)  # naredi abslutene zastopanosti 
-  n2 <- equalize(n2, n1)
-  d <- n1 - n2
-  s <- n1 + n2
-  d <- d[-length(d)]  # leave out unassigned
+# Function to calculate phi for each indivicual
+computephi_i <- function(n1, n2) {  # OTUs from t1 and t2 
+  #n1 <- equalize(n1, n2)  # from rel_abund to abs_abund  
+  #n2 <- equalize(n2, n1)
+  d <- n1 - n2  # difference between abs abund of OTU in time t1 and t2
+  s <- n1 + n2  # total between abs abund of OTU at time t1 and t2
+  d <- d[-length(d)]  # leave out unassigned # remove OTUs that do not have assigned taxonomy? 
   s <- s[-length(s)]  # leave out unassigned
-  phi <- (d^2 - s) / (s^2 - s)
+  phi <- (d^2 - s) / (s^2 - s) # calculate the phi value for an individual
   return(phi)
 }
 
+# Function to normalize relative abundances to 'absolute abundances'
 equalize <- function(n1, n2) {
-  n <- sum(n2)
+  n = sum(n2)
   if (sum(n1) <= n) {
     return(n1)
   } else {
@@ -76,49 +86,53 @@ equalize <- function(n1, n2) {
   }
 }
 
-# Split data by individual
-individuals = split(tab, tab$person)
-
-# Initialize the list to store time intervals for each individual
-T <- vector("list", 9)
-
 # Loop over each individual by name
 for (i in 1:9) {
-  times <- select(individuals[[i]], day)  # Assuming abd is now indexed by individual names
+  times <- select(tab[[i]], day)
   spans <- max(times) - min(times)
-  T[[i]] <- 1:span
+  T[[i]] <- 1:spans
 }
 
-# 
-Phi_i <- vector("list", length(tab))
-
 # For each individual for all combinations of time_points (days) calculate the dissimilarity 
-for (k in 1:9) { # za vsakega posazmenika vzamemo njegov data.frame 
-  times = select(individuals[[k]], day) # povelečemo ven vse časovne točke tega posameznika 
-  Phi_i[[k]] <- vector("list", length(T[[k]]))
-  for (i in 1:length(T[[k]])) {  # za vse možne časovne razlike 1 do 154 npr je 1, 2, 3, etc. 
-    for (t in 1:max(times) - T[[k]][[i]]) { # za vse vrednosti max dan - 1, 2, 3, 4, etc. 
-      t1 = individuals[[k]]$day == t  # poglej ali posameznik ima to točko 
-      t2 = individuals[[k]]$day == t + T[[k]][i] # in poglej ali posameznik ima točko, ki se razlikuje za T od te točke?
-      if (any(t1) && any(t2)) {  # Check if both t1 and t2 have any true values
-        if ("day" %in% colnames(individuals[[k]]) && "person" %in% colnames(individuals[[k]])) {
-          n1 <- individuals[[k]][t1, -which(names(individuals[[k]]) %in% c("day", "person"))]
-          n2 <- individuals[[k]][t2, -which(names(individuals[[k]]) %in% c("day", "person"))]
-        } else {
-          n1 <- individuals[[k]][t1, ]
-          n2 <- individuals[[k]][t2, ]
+# Take a data.frame for each individual
+for (k in 1:9) { 
+  # extract all time-points
+  times = select(tab[[k]], day) 
+  Phi_i[[k]] = vector("list", length(T[[k]]))  
+  # for all possible time differences from start to end of sampling period
+  for (i in 1:length(T[[k]])) {  
+    # for all the values from 1 to end of sampling period difference 
+    for (t in 1:max(times) - T[[k]][[i]]) {
+      # Does an individual have the OTU at theat time-point
+      t1 = which(times == t) 
+      # Does an individual have the OTU at the different time-points 
+      t2 = which(times == (t + T[[k]][i]))
+      # If both are present 
+      if (length(t1) > 0 && length(t2) > 0) { 
+        # remove columns day and person
+          n1 = tab[[k]][t1, -1]
+          n2 = tab[[k]][t2, -1]
         }
+        # Calculate Phi value 
       Phi_i[[k]][[i]] = rbind(Phi_i[[k]][[i]], computephi_i(n1, n2)) # Append result of computephi_i to Phi_i
-      }
     }
   }
 }
 
+# Calculate the mean phi
 meanPhi = vector('list', 9)
 
-for (k in 1:9) {
-  number_otus = ncol(individuals[[k]]) - 2  # count the number of OTUs in a data.frame and -2 is for the columns day and person
-  meanPhi[[k]] = matrix(NA, nrow = number_otus, ncol = length(T[[k]] + 2))
+for (k in 1:16) {
+  # number of OTUs
+  N_OTU <- ncol(Phi_i[[k]][[1]])
+  meanPhi[[k]] <- matrix(NA, nrow = N_OTU, ncol = length(T[[k]]) + 1)
+  for (i in 1:(length(T[[k]]) + 1)) {
+    if (nrow(Phi_i[[k]][[i]]) > 5) {
+      meanPhi[[k]][, i] <- colMeans(Phi_i[[k]][[i]], na.rm = TRUE)
+    } else {
+      meanPhi[[k]][, i] <- rep(NA, N_OTU)
+    }
+  }
 }
 
 ## Again my understanding of the code 
