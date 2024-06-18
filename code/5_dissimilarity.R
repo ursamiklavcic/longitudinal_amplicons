2479f26f990c1aa47ddedb668662ef0dbce4f2d3
Urša Miklavčič <ursa.miklavcic@gmail.com>
2024-06-18 14:26:31 +0000

    added time_lags

--- code/5_dissimilarity.R
+++ code/5_dissimilarity.R
@@ -23,6 +23,7 @@ theme_set(theme_bw())
 otutab_absrel = readRDS('data/r_data/otutab_absabund.RDS')
 otutabEM = readRDS('data/r_data/otutabEM.RDS')
 metadata = readRDS('data/r_data/metadata.RDS')
+taxtab = readRDS('data/r_data/taxonomy.RDS')
 
 # Are there drops in relative abundance in time?
 otutab_absrel %>% 
@@ -55,7 +56,7 @@ calculate_dissimilarity = function(tab, T){
  
   for (t in 1:(length(tab) - T)) {
     n_t <- tab[t]
-    n_t_plus_T <- tab[t+T]
+    n_t_plus_T <- tab[t + T]
     d_plus <- n_t + n_t_plus_T
     d_minus <- n_t - n_t_plus_T
     diss <- ((d_minus^2) - d_plus) / (d_plus * (d_plus - 1))
@@ -69,7 +70,7 @@ calculate_dissimilarity = function(tab, T){
 
 # Initialize results 
 results <- data.frame(mean_diss = numeric(), 
-                      time_lag = as.numeric(), 
+                      time_lag = numeric(), 
                       name = character(),
                       biota = character(),
                       person = character())
@@ -79,19 +80,21 @@ for ( b in unique(otutab_meta$biota)) {
   for (p in unique(otutab_meta$person)) {
     for (o in colnames(otutabEM)) {
       
-      tab <- otutab_meta %>%
+        tab <- otutab_meta %>%
         filter(biota == b & person == p) %>%
         arrange(day) %>%
         pull(o)
-      
-      mean_dissimilarity <- calculate_dissimilarity(tab, T)
-      
-      results <- rbind(results, cbind(mean_diss = mean_dissimilarity,
+        for(T in 1:length(tab)) {
+          if(sum(tab) > 0) { 
+            mean_dissimilarity <- calculate_dissimilarity(tab, T)
+        
+            results <- rbind(results, cbind(mean_diss = mean_dissimilarity,
                                             time_lag = as.numeric(T), 
                                             name = o, 
                                             person = p, 
-                                            biota = b))
-                             
+                                            biota = b)) 
+        }
+      }                       
     }
   }
 }
@@ -102,22 +105,35 @@ results
 # Plot mean dissimilarity in ethanol resistant fraction vs microbiota 
 ggplot(results, aes(x = time_lag, y = mean_diss, color = biota)) +
   geom_line() +
-  facet_wrap(vars(person))
+  facet_wrap(vars(biota))
 
 # what about different people? 
+ggplot(results, aes(x = time_lag, y = mean_diss, color = biota)) +
+  geom_line() +
+  facet_wrap(vars(people))
 
+# Mean dissimilarity of OTUs spore-forming and not
+ggplot(results, aes(x = biota, y = mean_diss)) +
+  geom_boxplot()
+
+# Taxonomy 
+results_tax = results %>% 
+  left_join(taxtab, by = 'name')
+
+
+# Relative abundance 
 # What about OTUs that are sporeforming and active, so were found in microbiota also? 
 
 
 
 
+## For later 
 # Dissimilarity between OTUs in the same community
-
-calculate_dissimilarity_between_communities <- function(afd_temporal_1, afd_temporal_2, T) {
+calculate_dissimilarity_between_communities <- function(tab_1, tab_2, T) {
   diss_all <- c()
-  for (t in 1:(length(afd_temporal_1) - T)) {
-    n_1_t <- afd_temporal_1[t]
-    n_2_t_plus_T <- afd_temporal_2[t + T]
+  for (t in 1:(length(tab_1) - T)) {
+    n_1_t <- tab_1[t]
+    n_2_t_plus_T <- tab_2[t + T]
     d_plus <- n_1_t + n_2_t_plus_T
     d_minus <- n_1_t - n_2_t_plus_T
     diss <- ((d_minus^2) - d_plus) / (d_plus * (d_plus - 1))
@@ -126,147 +142,3 @@ calculate_dissimilarity_between_communities <- function(afd_temporal_1, afd_temp
   mean_diss <- mean(diss_all)
   return(mean_diss)
 }
-
-
-# prepare to remove OTUs that are not present in all time-points
-otus_present = otutab_absrel %>%
-  left_join(select(metadata, Group, biota, person, day), by = 'Group') %>%
-  group_by(Group) %>%
-  mutate(PA = ifelse(value > 0, 1, 0)) %>%
-  ungroup() %>%
-  group_by(biota, person, name) %>%
-  summarise(sumPA = sum(PA))
-
-#time_spans 
-spans = otutab_absrel %>%
-  left_join(select(metadata, Group, biota, person, day), by = 'Group') %>%
-  group_by(biota, person) %>%
-  summarise(span = max(day) - min(day), .groups = 'drop') %>%
-  group_by(biota, person) %>%
-  mutate(time_spans = list(1:span))
-
-dissimilarity_abs = otutab_absrel %>%
-  left_join(select(metadata, Group, biota, person, day), by = 'Group') %>%
-  left_join(otus_present, by = c('biota', 'person', 'name')) %>%
-  left_join(spans, by = c('biota', 'person')) %>%
-  # remove OTUs that are not present in all time-points
-  filter(sumPA == 12) %>%
-  select(-sumPA) %>%
-  # for every fraction, person and OTU 
-  group_by(biota, person, name) %>%
-  # how to calculate d and s not just for 1 time-point back but all time-points
-  arrange(day, .by_group = TRUE) %>%
-  mutate(d = lead(abs_abund_ng) - abs_abund_ng, 
-         s = lead(abs_abund_ng) + abs_abund_ng, 
-         phi = (d^2 - s) / (s^2 - s)) %>%
-  ungroup()
-  
-ggplot(dissimilarity_abs, aes(x=day, y=phi, color=name)) +
-  geom_line(show.legend = FALSE) +
-  facet_wrap(vars(biota), nrow = 2, scales = 'free_y')
-
-
-  
-  
-
-  
-
-
-# Import metadata
-metadata = as_tibble(read.csv('data/metadata.csv', sep=';')) %>%
-  mutate(date=dmy(date))
-
-# Import otutab 
-otutabEM = readRDS('data/r_data/otutabEM.RDS')
-
-# Import ddPCR 
-ddPCR = readRDS('data/r_data/ddPCR.RDS')
-
-# load rel_abundce table and prepare for calculation 
-otutabEM_allabund = as.data.frame(otutabEM) %>% 
-  rownames_to_column('Group') %>%
-  left_join(select(ddPCR, Sample, CopiesPerSample) %>%
-              filter(Sample != 'NTC'), by = join_by('Group' == 'Sample')) %>%
-  pivot_longer(names_to = 'name', values_to = 'count', cols = starts_with('Otu')) %>%
-  group_by(Group) %>%
-  mutate(relabund = count/sum(count),
-         absabund = relabund*CopiesPerSample) %>%
-  ungroup() %>%
-  select(-CopiesPerSample)
-
-otutabM_allabund = filter(otutabEM_allabund, substr(Group, 1, 1) == 'M')
-
-tab1 = otutabM_allabund %>%
-  select(Group, name, absabund) %>%
-  pivot_wider(names_from = 'name', values_from = 'absabund') %>%
-  left_join(select(metadata, Group, day, person)) %>%
-  column_to_rownames('Group') 
-
-# move columns day and person the the frot of the table 
-tab2 = tab1[, c(9591, 9590, 1:9589)]
-# Split data by individual
-tab = split(tab2, tab2$person)
-
-for (i in 1:9) {
-  tab[[i]] = select(tab[[i]], -person)
-}
-
-
-
-# Loop over each individual by name
-for (i in 1:9) {
-  times <- select(tab[[i]], day)
-  spans <- max(times) - min(times)
-  T[[i]] <- 1:spans
-}
-
-# For each individual for all combinations of time_points (days) calculate the dissimilarity 
-# Take a data.frame for each individual
-for (k in 1:9) { 
-  # extract all time-points
-  times = select(tab[[k]], day) 
-  Phi_i[[k]] = vector("list", length(T[[k]]))  
-  # for all possible time differences from start to end of sampling period
-  for (i in 1:length(T[[k]])) {  
-    # for all the values from 1 to end of sampling period difference 
-    for (t in 1:max(times) - T[[k]][[i]]) {
-      # Does an individual have the OTU at theat time-point
-      t1 = which(times == t) 
-      # Does an individual have the OTU at the different time-points 
-      t2 = which(times == (t + T[[k]][i]))
-      # If both are present 
-      if (length(t1) > 0 && length(t2) > 0) { 
-        # remove columns day and person
-          n1 = tab[[k]][t1, -1]
-          n2 = tab[[k]][t2, -1]
-        }
-        # Calculate Phi value 
-      Phi_i[[k]][[i]] = rbind(Phi_i[[k]][[i]], computephi_i(n1, n2)) # Append result of computephi_i to Phi_i
-    }
-  }
-}
-
-# Calculate the mean phi
-meanPhi = vector('list', 9)
-
-for (k in 1:16) {
-  # number of OTUs
-  N_OTU <- ncol(Phi_i[[k]][[1]])
-  meanPhi[[k]] <- matrix(NA, nrow = N_OTU, ncol = length(T[[k]]) + 1)
-  for (i in 1:(length(T[[k]]) + 1)) {
-    if (nrow(Phi_i[[k]][[i]]) > 5) {
-      meanPhi[[k]][, i] <- colMeans(Phi_i[[k]][[i]], na.rm = TRUE)
-    } else {
-      meanPhi[[k]][, i] <- rep(NA, N_OTU)
-    }
-  }
-}
-
-
-
-
-
-
-
-
-
