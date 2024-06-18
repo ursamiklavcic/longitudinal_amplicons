5b7d55d115cc097c6dcbe38f8db5bed4730cb88c
Urša Miklavčič <ursa.miklavcic@gmail.com>
2024-06-18 13:13:22 +0000

    Dissimilarity for each OTU in each host across all time points code

--- code/5_dissimilarity.R
+++ code/5_dissimilarity.R
@@ -13,10 +13,16 @@
 library(cli, lib.loc = "/home/nlzoh.si/ursmik1/R/x86_64-pc-linux-gnu-library/4.1")
 library(rlang, lib.loc = "/home/nlzoh.si/ursmik1/R/x86_64-pc-linux-gnu-library/4.1")
 library(dplyr)
+library(tibble)
 library(ggplot2)
 
 set.seed(96)
 theme_set(theme_bw())
+
+# Import data 
+otutab_absrel = readRDS('data/r_data/otutab_absabund.RDS')
+otutabEM = readRDS('data/r_data/otutabEM.RDS')
+metadata = readRDS('data/r_data/metadata.RDS')
 
 # Are there drops in relative abundance in time?
 otutab_absrel %>% 
@@ -34,8 +40,85 @@ ggsave('out/ethanol_resistantVSmicrobiota/relabund_time.png', dpi=600)
 # calculate all possible time-spans for an OTU of an individual 
 # calculate difference bwteen abs abund of OTU and abs abund of OTU + difference to all other time-points 
 # calculate sum bwteen abs abund of OTU and abs abund of OTU + difference to all other time-points
-# divide this two and square them 
+# divide this two and square them
 
+# Prepare the data 
+otutab_meta = otutabEM %>% 
+  as.data.frame() %>%
+  rownames_to_column('Group') %>%
+  left_join(metadata, by = 'Group') 
+
+# Function to calculate dissimilarity for an OTU from 1 host across all time-points
+calculate_dissimilarity = function(tab, T){
+  diss_all <- c()
+  #diss_mean <- data.frame()
+  diss_mean <- c()
+ 
+  for (t in 1:(length(tab) - T)) {
+    n_t <- tab[t]
+    n_t_plus_T <- tab[t+T]
+    d_plus <- n_t + n_t_plus_T
+    d_minus <- n_t - n_t_plus_T
+    diss <- ((d_minus^2) - d_plus) / (d_plus * (d_plus - 1))
+    diss_all <- cbind(diss_all, diss)
+    
+  }
+  diss_mean <- c(mean(diss_all, na.rm= TRUE))
+  #diss_mean <- data.frame(mean_diss = mean(diss_all), 
+  #                       time_lag = T)
+  return(diss_mean)
+}
+
+# Initialize results 
+results <- data.frame(mean_diss = numeric(), 
+                      time_lag = as.numeric(), 
+                      name = character(),
+                      biota = character(),
+                      person = character())
+
+# for loops for all Otus, from different hosts, different biotas! 
+for ( b in unique(otutab_meta$biota)) {
+  for (p in unique(otutab_meta$person)) {
+    for (o in colnames(otutabEM)) {
+      
+      tab <- otutab_meta %>%
+        filter(biota == b & person == p) %>%
+        arrange(day) %>%
+        pull(o)
+      
+      mean_dissimilarity <- calculate_dissimilarity(tab, T)
+      
+      results <- rbind(results, cbind(mean_diss = mean_dissimilarity,
+                                            time_lag = as.numeric(T), 
+                                            name = o, 
+                                            person = p, 
+                                            biota = b))
+                             
+    }
+  }
+}
+
+results
+
+ggplot(results, aes(x = time_lag, y = mean_diss, color = biota)) +
+  geom_line() +
+  facet_wrap(vars(person))
+
+
+
+calculate_dissimilarity_between_communities <- function(afd_temporal_1, afd_temporal_2, T) {
+  diss_all <- c()
+  for (t in 1:(length(afd_temporal_1) - T)) {
+    n_1_t <- afd_temporal_1[t]
+    n_2_t_plus_T <- afd_temporal_2[t + T]
+    d_plus <- n_1_t + n_2_t_plus_T
+    d_minus <- n_1_t - n_2_t_plus_T
+    diss <- ((d_minus^2) - d_plus) / (d_plus * (d_plus - 1))
+    diss_all <- c(diss_all, diss)
+  }
+  mean_diss <- mean(diss_all)
+  return(mean_diss)
+}
 
 
 # prepare to remove OTUs that are not present in all time-points
