8a67739b2e68632de8c67aef74c6c0c077033276
Urša Miklavčič <ursa.miklavcic@gmail.com>
2024-06-19 10:07:49 +0000

    Dissimilarity graphs

--- code/5_dissimilarity.R
+++ code/5_dissimilarity.R
@@ -18,6 +18,10 @@ library(ggplot2)
 
 set.seed(96)
 theme_set(theme_bw())
+# Colors 
+cole=c('#47B66A') # green 
+colm=c('#D9A534') # yellow
+colem= c('#47B66A', '#D9A534')
 
 # Import data 
 otutab_absrel = readRDS('data/r_data/otutab_absabund.RDS')
@@ -25,15 +29,28 @@ otutabEM = readRDS('data/r_data/otutabEM.RDS')
 metadata = readRDS('data/r_data/metadata.RDS')
 taxtab = readRDS('data/r_data/taxonomy.RDS')
 
+# Always present OTUs 
+present_otus = otutabEM %>% 
+  as.data.frame() %>%
+  rownames_to_column('Group') %>%
+  pivot_longer(-Group) %>%
+  mutate(PA = ifelse(value > 0, 1, 0)) %>%
+  left_join(metadata, by = 'Group') %>%
+  group_by(biota, person, name) %>%
+  reframe(sum_otu = sum(PA), 
+          sum_sample = n_distinct(Group)) %>%
+  filter(sum_otu == sum_sample) %>%
+  select(-sum_otu, -sum_sample)
+
 # Are there drops in relative abundance in time?
 otutab_absrel %>% 
-  filter(substr(Group, 1, 1) == 'M') %>% 
-  filter(rel_abund > 0.001) %>%
   left_join(metadata, by = 'Group') %>%
+  right_join(present_otus, by = c('biota', 'person', 'name')) %>%
   ggplot(aes(x = day, y = rel_abund, color = name)) +
   geom_line(show.legend = FALSE) +
   scale_y_log10() +
-  facet_wrap(vars(person)) 
+  facet_grid(person ~ biota, scales = 'free_y') +
+  labs(x = 'Day', y = 'log10(relative abundance)')
 ggsave('out/ethanol_resistantVSmicrobiota/relabund_time.png', dpi=600)
 
 # Calculateing stationary/stohastic OTU 
@@ -43,11 +60,19 @@ ggsave('out/ethanol_resistantVSmicrobiota/relabund_time.png', dpi=600)
 # calculate sum bwteen abs abund of OTU and abs abund of OTU + difference to all other time-points
 # divide this two and square them
 
-# Prepare the data 
-otutab_meta = otutabEM %>% 
+# Remove OTUs that are not always present
+otutab_meta = otutabEM %>%
   as.data.frame() %>%
   rownames_to_column('Group') %>%
-  left_join(metadata, by = 'Group') 
+  left_join(metadata, by = 'Group') %>%
+  pivot_longer(names_to = 'name', values_to = 'count', cols = starts_with('Otu')) %>%
+  right_join(present_otus, by = c('biota', 'person', 'name')) %>%
+  pivot_wider(names_from = 'name', values_from = 'count', values_fill = 0)
+
+otutabEM_present = otutab_meta %>%
+  select(Group, starts_with('Otu')) %>%
+  column_to_rownames('Group') %>%
+  as.matrix()
 
 # Function to calculate dissimilarity for an OTU from 1 host across all time-points
 calculate_dissimilarity = function(tab, T){
@@ -63,7 +88,7 @@ calculate_dissimilarity = function(tab, T){
     diss_all <- cbind(diss_all, diss)
     
   }
-  diss_mean <- c(mean(diss_all, na.rm= TRUE))
+  diss_mean <- c(mean(diss_all))
   
   return(diss_mean)
 }
@@ -76,20 +101,21 @@ results <- data.frame(mean_diss = numeric(),
                       person = character())
 
 # for loops for all Otus, from different hosts, different biotas! 
-for ( b in unique(otutab_meta$biota)) {
+for (b in unique(otutab_meta$biota)) {
   for (p in unique(otutab_meta$person)) {
-    for (o in colnames(otutabEM)) {
+    for (o in colnames(otutabEM_present)) {
       
         tab <- otutab_meta %>%
         filter(biota == b & person == p) %>%
         arrange(day) %>%
         pull(o)
+        
         for(T in 1:length(tab)) {
           if(sum(tab) > 0) { 
             mean_dissimilarity <- calculate_dissimilarity(tab, T)
         
             results <- rbind(results, cbind(mean_diss = mean_dissimilarity,
-                                            time_lag = as.numeric(T), 
+                                            time_lag = T, 
                                             name = o, 
                                             person = p, 
                                             biota = b)) 
@@ -99,29 +125,44 @@ for ( b in unique(otutab_meta$biota)) {
   }
 }
 
-results
+dissimilarity = na.omit(results) %>%
+  mutate(mean_diss = as.numeric(mean_diss), 
+         time_lag = as.numeric(time_lag)*14) %>%
+  left_join(taxtab, by ='name')
 
 # Plots
 # Plot mean dissimilarity in ethanol resistant fraction vs microbiota 
-ggplot(results, aes(x = time_lag, y = mean_diss, color = biota)) +
-  geom_line() +
-  facet_wrap(vars(biota))
+ggplot(dissimilarity, aes(x = time_lag, y = mean_diss, color = name)) +
+  geom_line(show.legend = FALSE) +
+  scale_x_continuous(breaks = seq(0,200, 50)) +
+  facet_wrap(vars(biota)) +
+  labs(x = 'T(days)', y = 'Dissimilarity at lag T')
+# Dissimilarity beyond the time_lag 5, as I see from my data from before that there the acqusition of new 
+# OTUs becomes stable(does not increase any more)
+
+dissimilarity %>%
+  filter(time_lag > (5*14)) %>%
+  ggplot(aes(x = time_lag, y = mean_diss, color = name)) +
+  geom_line(show.legend = FALSE) +
+  facet_grid(~biota) +
+  labs(x = 'T(days)', y = 'Dissimilarity at lag T')
 
 # what about different people? 
-ggplot(results, aes(x = time_lag, y = mean_diss, color = biota)) +
-  geom_line() +
-  facet_wrap(vars(people))
+ggplot(dissimilarity, aes(x = time_lag, y = mean_diss, color = name)) +
+  geom_line(show.legend = FALSE) +
+  scale_x_continuous(breaks = seq(0,200, 50)) +
+  facet_grid(biota ~person) +
+  labs(x = 'T(days)', y = 'Dissimilarity at lag T')
 
-# Mean dissimilarity of OTUs spore-forming and not
-ggplot(results, aes(x = biota, y = mean_diss)) +
-  geom_boxplot()
+# Mean difference between dissimilarity in ethanol resistant fraction VS microbiota
+ggplot(dissimilarity, aes(x = biota, y= mean_diss, fill = biota)) +
+  geom_boxplot() + 
+  scale_fill_manual(values = colem) +
+  labs(x = '', y = 'Dissimilarity', fill= '')
 
-# Taxonomy 
-results_tax = results %>% 
-  left_join(taxtab, by = 'name')
+# Identification of OTUs that are stationary or non-stationary?
 
 
-# Relative abundance 
 # What about OTUs that are sporeforming and active, so were found in microbiota also? 
 
 
