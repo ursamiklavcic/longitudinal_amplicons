bf618737e5d2e61e43dc9ad2c5fa81afa568e3a6
Urša Miklavčič <ursa.miklavcic@gmail.com>
2024-10-03 14:35:25 +0000

    Plot for C. difficile

--- code/2_absolute_quantification.R
+++ code/2_absolute_quantification.R
@@ -82,21 +82,26 @@ otutab_absrel = rownames_to_column(as.data.frame(otutabEM), 'Group') %>%
 saveRDS( otutab_absrel, 'data/r_data/otutab_absrel.RDS')
 
 # Additional experiment: Does PowerFecal kit isolate spores?
-samples = read_excel('data/absolute_quantification/PowerFecal//samples.xlsx')
+samples = read_excel('data/absolute_quantification/PowerFecal/samples.xlsx')
 
 pf = read.quantasoft('data/absolute_quantification/PowerFecal/ddpcr_cdiff_20240808_RESULTS.csv') %>%
   right_join(samples, by = 'Sample') %>%
-  mutate(Concentration = Concentration * (25/2.5) * (25/20) * 1000 ) %>%
-  mutate(Concentration = ifelse(sample_description == c('fecal sample', 'negative control (H2O)'), 0, Concentration))
+  mutate(abs_conc = Concentration * (25/2.5) * (25/20) * 1000) 
 
 pf$sample_description = factor(pf$sample_description, levels = c('negative control (H2O)', 'negative control (E. coli)','fecal sample', 'fecal sample + 10e-4 spores', 'fecal sample + 10e-3 spores', 
                                                         'fecal sample + 10e-1 spores', 'fecal sample with undiluted spores','positive control (C.diff)'))
 
-ggplot(pf, aes(x = sporeCFU, y = Concentration, color = sample_description )) +
+ggplot(pf_cor, aes(x = sporeCFU, y = abs_conc, color = sample_description )) +
   geom_point(size = 3) +
   scale_x_continuous(trans = log_trans(), breaks = c(0, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11)) +
-  scale_y_continuous(trans = log_trans(), breaks = c(1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10)) +
+  scale_y_continuous(trans = log_trans(), breaks = c(0, 1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10)) +
   geom_abline() +
-  labs(x = 'Spores CFU/ml', y = 'Copies of C. diff 16S rRNA/ml', color = 'Sample type')
-
+  labs(x = expression(italic("C. difficile") * " spores CFU/ml"), 
+       y = expression("Copies of " * italic("C. difficile") * " 16S rRNA/ml"), color = 'Sample')
 ggsave('out/exploration/PowerFecal_spores.png', dpi = 600)
+
+
+pf_cor <- pf %>%
+  filter(sample_description != 'negative control (H2O)' & sample_description != 'negative control (E. coli)' & sample_description !='positive control (C.diff)')
+  
+cor.test(pf_cor$Concentration, pf_cor$sporeCFU, method = 'pearson')
