b7e8edd94926cd22f121092f40936af3ff23c4d3
Urša Miklavčič <ursa.miklavcic@gmail.com>
2024-10-03 14:45:23 +0000

    added bootstraping

--- code/3_beta.R
+++ code/3_beta.R
@@ -88,69 +88,139 @@ bray <- as.matrix(dist_bray) %>%
 
 bray$fraction.y <- factor(bray$fraction.y , levels = c("Non-ethanol resistant OTUs", 'Non-ethanol resistant Bacillota',  "Ethanol resistant Bacillota", "Other ethanol resistant OTUs"))
 
-# Statistics 
-within <- filter(bray, same_person == "Within individual") %>%
-  distinct(Group, .keep_all = TRUE)
+# # Statistics 
+# within <- filter(bray, same_person == "Within individual") %>%
+#   distinct(Group, .keep_all = TRUE)
+# 
+# within_dist <- filter(bray, same_person == "Within individual") %>%
+#   select(Group, name, value) %>%
+#   pivot_wider(values_fill = 1) %>%
+#   column_to_rownames('Group') %>%
+#   as.dist()
+# anosim_within <- anosim(within_dist, within$fraction.y, permutations = 999)
+# 
+# between <- filter(bray, same_person == "Between individuals") %>%
+#   distinct(Group, .keep_all = TRUE) 
+# between_dist <- filter(bray, same_person == "Between individuals") %>%
+#   select(Group, name, value) %>%
+#   pivot_wider(values_fill = 1) %>%
+#   column_to_rownames('Group') %>%
+#   as.dist()
+# 
+# anosim_between <- anosim(between_dist, between$fraction.y, permutations = 999)
+# 
+# anotate <- data.frame(same_person = c('Within individual', 'Between individuals'),
+#                       fraction.y = c('Other ethanol resistant OTUs', 'Other ethanol resistant OTUs'),
+#                       significance = c(anosim_within$signif, anosim_between$signif), 
+#                       statistic = c(anosim_within$statistic, anosim_between$statistic))
 
-within_dist <- filter(bray, same_person == "Within individual") %>%
-  select(Group, name, value) %>%
-  pivot_wider(values_fill = 1) %>%
-  column_to_rownames('Group') %>%
-  as.dist()
-anosim_within <- anosim(within_dist, within$fraction.y, permutations = 999)
 
-between <- filter(bray, same_person == "Between individuals") %>%
-  distinct(Group, .keep_all = TRUE) 
-between_dist <- filter(bray, same_person == "Between individuals") %>%
-  select(Group, name, value) %>%
-  pivot_wider(values_fill = 1) %>%
-  column_to_rownames('Group') %>%
-  as.dist()
-
-anosim_between <- anosim(between_dist, between$fraction.y, permutations = 999)
-
-anotate <- data.frame(same_person = c('Within individual', 'Between individuals'),
-                      fraction.y = c('Other ethanol resistant OTUs', 'Other ethanol resistant OTUs'),
-                      significance = c(anosim_within$signif, anosim_between$signif), 
-                      statistic = c(anosim_within$statistic, anosim_between$statistic))
-
-# Pairwise ANOSIM 
-group_combinations <- combn(levels(bray$fraction.y), 2)
-
-anosim_res <- data.frame()
-
-# Loop through each combination of group pairs
-for (i in 1:ncol(group_combinations)) {
-  for (p in unique(bray$same_person)) {
-    # Define the number of fractions
-    fractions <- c(group_combinations[1, i], group_combinations[2, i])
-    # Filter same person or not
-    meta <- filter(bray, same_person == 'Within individual') %>%
-      distinct(Group, .keep_all = TRUE) %>%
-      filter(fraction.y %in% fractions)
+# Bootstraping ANOSIM 
+bootstrap_anosim_bray <- function(otu_all, otu_fraction, metadata, n_iterations = 1000) {
+  set.seed(96)  
+  # Dataframe to store results from each iteration
+  results <- data.frame(iteration = integer(),
+                        within_statistic = numeric(),
+                        within_significance = numeric(),
+                        between_statistic = numeric(),
+                        between_significance = numeric())
+  
+  # Get list of unique fractions
+  fractions <- unique(otu_fraction$fraction)
+  
+  # Perform bootstrapping
+  for (i in 1:n_iterations) {
+    resampled_otutab <- matrix()
     
-    dist <- filter(bray, same_person == "Within individual" & fraction.y %in% fractions) %>%
+    # Resample OTUs within each fraction using dplyr
+    resampled_otus <- otu_all %>%
+      group_by(fraction, Group) %>%
+      summarize(name = list(sample(unique(name), size = 326, replace = FALSE)), .groups = 'drop') %>%
+      unnest(name) %>%
+      left_join(select(otu_all, Group, name, fraction, value), by = c('Group', 'fraction', 'name'))
+    
+    # Create resampled OTU table by selecting the resampled OTU names
+    resampled_otutab <- select(resampled_otus,  Group, name, value) %>%
+      pivot_wider(names_from = 'name', values_from = 'value', values_fill = 0) %>%
+      column_to_rownames('Group')
+    
+    # Recalculate Bray-Curtis distances for resampled OTUs
+    dist_bray <- vegdist(resampled_otutab, method = 'bray')
+    
+    # Tidy the Bray-Curtis matrix
+    bray <- as.matrix(dist_bray) %>% 
+      as_tibble(rownames = 'Group') %>%
+      pivot_longer(-Group) %>%
+      filter(Group != name) %>%
+      left_join(otu_fraction, by = 'Group') %>%
+      left_join(otu_fraction, by = join_by('name' == 'Group')) %>%
+      mutate(Group_clean = str_remove(Group, "-.*$"), 
+             name_clean = str_remove(name, '-.*$')) %>%
+      left_join(metadata %>% select(Group, person, date), by = join_by('Group_clean' == 'Group')) %>%
+      left_join(metadata %>% select(Group, person, date), by = join_by('name_clean' == 'Group')) %>%
+      mutate(same_person = ifelse(person.x == person.y, 'Within individual', 'Between individuals'), 
+             same_fraction = ifelse(fraction.x == fraction.y, 'Yes', 'No')) %>%
+      filter(same_fraction == 'Yes')
+    
+    # Within-individual and between-individual distances
+    within <- filter(bray, same_person == "Within individual") %>%
+      distinct(Group, .keep_all = TRUE)
+    
+    within_dist <- filter(bray, same_person == "Within individual") %>%
       select(Group, name, value) %>%
       pivot_wider(values_fill = 1) %>%
       column_to_rownames('Group') %>%
       as.dist()
     
-    anosim <- anosim(dist, meta$fraction.y, permutations = 999)
-    anosim_stat <- data.frame(same_person = p, 
-                            fractions = paste(fractions, collapse = " & "), 
-                            statistic = anosim$statistic, 
-                            significance = anosim$signif)
-    anosim_res <- rbind(anosim_res, anosim_stat)
-  
-    }
+    between <- filter(bray, same_person == "Between individuals") %>%
+      distinct(Group, .keep_all = TRUE) 
+    
+    between_dist <- filter(bray, same_person == "Between individuals") %>%
+      select(Group, name, value) %>%
+      pivot_wider(values_fill = 1) %>%
+      column_to_rownames('Group') %>%
+      as.dist()
+    
+    # Run ANOSIM for within and between individuals
+    anosim_within <- anosim(within_dist, within$fraction.y, permutations = 999)
+    anosim_between <- anosim(between_dist, between$fraction.y, permutations = 999)
+    
+    # Store results from this iteration
+    results <- rbind(results, data.frame(
+      iteration = i,
+      within_statistic = anosim_within$statistic,
+      within_significance = anosim_within$signif,
+      between_statistic = anosim_between$statistic,
+      between_significance = anosim_between$signif
+    ))
   }
+  
+  return(results)
+}
 
-anosim_res$adjusted_p <- p.adjust(anosim_res$significance, method = "BH")
 
+# Run the OTU resampling function with 1000 iterations
+bootstrap_results <- bootstrap_anosim_by_fraction(otu_all, otu_fraction, metadata, n_iterations = 999)
+saveRDS(bootstrap_results, 'out/bootstrap_results_bray.RDS')
 
-# # Pairwise Wilcox
-# wilcox_within <- pairwise.wilcox.test(within$value, within$fraction.y, p.adjust.method = 'BH')
-# wilcox_between <- pairwise.wilcox.test(between$value, between$fraction.y, p.adjust.method = 'BH')
+# Summarize results
+bootstrap_summary <- bootstrap_results %>%
+  summarise(within_mean_statistic = mean(within_statistic),
+            within_sd_statistic = sd(within_statistic),
+            between_mean_statistic = mean(between_statistic),
+            between_sd_statistic = sd(between_statistic),
+            within_mean_significance = mean(within_significance),
+            within_sd_significance = sd(within_significance),
+            between_mean_significance = mean(between_significance),
+            between_sd_significance = sd(between_significance))
+
+anotate <- data.frame(same_person = c('Within individual', 'Between individuals'),
+                      fraction.y = c('Non-ethanol resistant OTUs', 'Non-ethanol resistant OTUs'),
+                      significance = c(bootstrap_summary$within_mean_significance , bootstrap_summary$between_mean_significance), 
+                      statistic = c(bootstrap_summary$within_mean_statistic , bootstrap_summary$between_mean_statistic),
+                      sd_significance = c(bootstrap_summary$within_sd_significance , bootstrap_summary$between_sd_significance), 
+                      sd_statistic = c(bootstrap_summary$within_sd_statistic, bootstrap_summary$between_sd_statistic)) %>%
+  mutate(significance = as.numeric(significance))
 
 # Plot 
 bray_boxplot <- bray %>%
@@ -302,69 +372,139 @@ jaccard <- as.matrix(dist_jaccard) %>%
 
 jaccard$fraction.y <- factor(jaccard$fraction.y , levels = c("Non-ethanol resistant OTUs", 'Non-ethanol resistant Bacillota',  "Ethanol resistant Bacillota", "Other ethanol resistant OTUs"))
 
-# Statistics 
-within <- filter(jaccard, same_person == "Within individual") %>%
-  distinct(Group, .keep_all = TRUE)
+# # Statistics 
+# within <- filter(jaccard, same_person == "Within individual") %>%
+#   distinct(Group, .keep_all = TRUE)
+# 
+# within_dist <- filter(jaccard, same_person == "Within individual") %>%
+#   select(Group, name, value) %>%
+#   pivot_wider(values_fill = 1) %>%
+#   column_to_rownames('Group') %>%
+#   as.dist()
+# anosim_within <- anosim(within_dist, within$fraction.y, permutations = 999)
+# 
+# between <- filter(jaccard, same_person == "Between individuals") %>%
+#   distinct(Group, .keep_all = TRUE) 
+# between_dist <- filter(jaccard, same_person == "Between individuals") %>%
+#   select(Group, name, value) %>%
+#   pivot_wider(values_fill = 1) %>%
+#   column_to_rownames('Group') %>%
+#   as.dist()
+# 
+# anosim_between <- anosim(between_dist, between$fraction.y, permutations = 999)
+# 
+# anotate <- data.frame(same_person = c('Within individual', 'Between individuals'),
+#                       fraction.y = c('Other ethanol resistant OTUs', 'Other ethanol resistant OTUs'),
+#                       significance = c(anosim_within$signif, anosim_between$signif), 
+#                       statistic = c(anosim_within$statistic, anosim_between$statistic))
 
-within_dist <- filter(jaccard, same_person == "Within individual") %>%
-  select(Group, name, value) %>%
-  pivot_wider(values_fill = 1) %>%
-  column_to_rownames('Group') %>%
-  as.dist()
-anosim_within <- anosim(within_dist, within$fraction.y, permutations = 999)
-
-between <- filter(jaccard, same_person == "Between individuals") %>%
-  distinct(Group, .keep_all = TRUE) 
-between_dist <- filter(jaccard, same_person == "Between individuals") %>%
-  select(Group, name, value) %>%
-  pivot_wider(values_fill = 1) %>%
-  column_to_rownames('Group') %>%
-  as.dist()
-
-anosim_between <- anosim(between_dist, between$fraction.y, permutations = 999)
-
-anotate <- data.frame(same_person = c('Within individual', 'Between individuals'),
-                      fraction.y = c('Other ethanol resistant OTUs', 'Other ethanol resistant OTUs'),
-                      significance = c(anosim_within$signif, anosim_between$signif), 
-                      statistic = c(anosim_within$statistic, anosim_between$statistic))
-
-# Pairwise ANOSIM 
-group_combinations <- combn(levels(jaccard$fraction.y), 2)
-anosim_res <- data.frame()
-
-# Loop through each combination of group pairs
-for (i in 1:ncol(group_combinations)) {
-  for (p in unique(jaccard$same_person)) {
-    # Define the number of fractions
-    fractions <- c(group_combinations[1, i], group_combinations[2, i])
-    # Filter same person or not
-    meta <- filter(jaccard, same_person == 'Within individual') %>%
-      distinct(Group, .keep_all = TRUE) %>%
-      filter(fraction.y %in% fractions)
+# Bootstraping ANOSIM 
+bootstrap_anosim_jaccard <- function(otu_all, otu_fraction, metadata, n_iterations = 999) {
+  set.seed(96)  
+  # Dataframe to store results from each iteration
+  results <- data.frame(iteration = integer(),
+                        within_statistic = numeric(),
+                        within_significance = numeric(),
+                        between_statistic = numeric(),
+                        between_significance = numeric())
+  
+  # Get list of unique fractions
+  fractions <- unique(otu_fraction$fraction)
+  
+  # Perform bootstrapping
+  for (i in 1:n_iterations) {
+    resampled_otutab <- matrix()
     
+    # Resample OTUs within each fraction using dplyr
+    resampled_otus <- otu_all %>%
+      group_by(fraction, Group) %>%
+      summarize(name = list(sample(unique(name), size = 326, replace = FALSE)), .groups = 'drop') %>%
+      unnest(name) %>%
+      left_join(select(otu_all, Group, name, fraction, value), by = c('Group', 'fraction', 'name'))
     
-    dist <- filter(jaccard, same_person == "Within individual" & fraction.y %in% fractions) %>%
+    # Create resampled OTU table by selecting the resampled OTU names
+    resampled_otutab <- select(resampled_otus,  Group, name, value) %>%
+      pivot_wider(names_from = 'name', values_from = 'value', values_fill = 0) %>%
+      column_to_rownames('Group')
+    
+    # Recalculate jaccard-Curtis distances for resampled OTUs
+    dist_jaccard <- vegdist(resampled_otutab, method = 'jaccard')
+    
+    # Tidy the jaccard matrix
+    jaccard <- as.matrix(dist_jaccard) %>% 
+      as_tibble(rownames = 'Group') %>%
+      pivot_longer(-Group) %>%
+      filter(Group != name) %>%
+      left_join(otu_fraction, by = 'Group') %>%
+      left_join(otu_fraction, by = join_by('name' == 'Group')) %>%
+      mutate(Group_clean = str_remove(Group, "-.*$"), 
+             name_clean = str_remove(name, '-.*$')) %>%
+      left_join(metadata %>% select(Group, person, date), by = join_by('Group_clean' == 'Group')) %>%
+      left_join(metadata %>% select(Group, person, date), by = join_by('name_clean' == 'Group')) %>%
+      mutate(same_person = ifelse(person.x == person.y, 'Within individual', 'Between individuals'), 
+             same_fraction = ifelse(fraction.x == fraction.y, 'Yes', 'No')) %>%
+      filter(same_fraction == 'Yes')
+    
+    # Within-individual and between-individual distances
+    within <- filter(jaccard, same_person == "Within individual") %>%
+      distinct(Group, .keep_all = TRUE)
+    
+    within_dist <- filter(jaccard, same_person == "Within individual") %>%
       select(Group, name, value) %>%
       pivot_wider(values_fill = 1) %>%
       column_to_rownames('Group') %>%
       as.dist()
     
-    anosim <- anosim(dist, meta$fraction.y, permutations = 999)
-    anosim_stat <- data.frame(same_person = p, 
-                              fractions = paste(fractions, collapse = " & "), 
-                              statistic = anosim$statistic, 
-                              significance = anosim$signif)
-    anosim_res <- rbind(anosim_res, anosim_stat)
+    between <- filter(jaccard, same_person == "Between individuals") %>%
+      distinct(Group, .keep_all = TRUE) 
     
+    between_dist <- filter(jaccard, same_person == "Between individuals") %>%
+      select(Group, name, value) %>%
+      pivot_wider(values_fill = 1) %>%
+      column_to_rownames('Group') %>%
+      as.dist()
+    
+    # Run ANOSIM for within and between individuals
+    anosim_within <- anosim(within_dist, within$fraction.y, permutations = 999)
+    anosim_between <- anosim(between_dist, between$fraction.y, permutations = 999)
+    
+    # Store results from this iteration
+    results <- rbind(results, data.frame(
+      iteration = i,
+      within_statistic = anosim_within$statistic,
+      within_significance = anosim_within$signif,
+      between_statistic = anosim_between$statistic,
+      between_significance = anosim_between$signif
+    ))
   }
+  
+  return(results)
 }
 
-anosim_res$adjusted_p <- p.adjust(anosim_res$significance, method = "BH")
 
+# Run the OTU resampling function with 1000 iterations
+bootstrap_results <- bootstrap_anosim_by_fraction(otu_all, otu_fraction, metadata, n_iterations = 999)
+saveRDS(bootstrap_results, 'out/bootstrap_results_jaccard.RDS')
 
-# # Pairwise Wilcox
-# wilcox_within <- pairwise.wilcox.test(within$value, within$fraction.y, p.adjust.method = 'BH')
-# wilcox_between <- pairwise.wilcox.test(between$value, between$fraction.y, p.adjust.method = 'BH')
+# Summarize results
+bootstrap_summary <- bootstrap_results %>%
+  summarise(within_mean_statistic = mean(within_statistic),
+            within_sd_statistic = sd(within_statistic),
+            between_mean_statistic = mean(between_statistic),
+            between_sd_statistic = sd(between_statistic),
+            within_mean_significance = mean(within_significance),
+            within_sd_significance = sd(within_significance),
+            between_mean_significance = mean(between_significance),
+            between_sd_significance = sd(between_significance))
+
+anotate <- data.frame(same_person = c('Within individual', 'Between individuals'),
+                      fraction.y = c('Non-ethanol resistant OTUs', 'Non-ethanol resistant OTUs'),
+                      significance = c(bootstrap_summary$within_mean_significance , bootstrap_summary$between_mean_significance), 
+                      statistic = c(bootstrap_summary$within_mean_statistic , bootstrap_summary$between_mean_statistic),
+                      sd_significance = c(bootstrap_summary$within_sd_significance , bootstrap_summary$between_sd_significance), 
+                      sd_statistic = c(bootstrap_summary$within_sd_statistic, bootstrap_summary$between_sd_statistic)) %>%
+  mutate(significance = as.numeric(significance))
+
 
 # Plot 
 jaccard_boxplot <- jaccard %>%
