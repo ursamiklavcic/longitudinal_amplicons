13b6e09579166db19c19afb276a107b38494a9ca
Urša Miklavčič <ursa.miklavcic@gmail.com>
2024-11-12 13:42:27 +0000

    Defining the etoh resistant fractions

---
+++ code/1_define_etoh.R
@@ -0,0 +1,143 @@
+#Load environemnt
+
+# renv::init()
+# renv::snapshot()
+renv::restore()
+
+# # Load libraries
+# library(renv)
+# library(readxl)
+# library(ggplot2)
+# library(tidyr)
+# library(dplyr)
+# library(vegan)
+# library(tibble)
+# library(scales)
+# library(phyloseq)
+# library(car) # dependency for ggpubr
+# library(ggpubr)
+
+
+set.seed(96)
+theme_set(theme_bw())
+
+# Import data 
+metadata <- readRDS('data/r_data/metadata.RDS')
+otutabEM <- readRDS('data/r_data/otutabEM.RDS')
+taxtab <- readRDS('data/r_data/taxtab.RDS')
+seqtab <- readRDS('data/r_data/seqtab.RDS')
+seq_taxtab <- readRDS('data/r_data/seq_taxtab.RDS')
+tree <- readRDS('data/r_data/tree.RDS')
+
+otutab_absrel <- readRDS('data/r_data/otutab_absrel.RDS')
+
+set.seed(96)
+theme_set(theme_bw())
+
+# Colors to be used
+col_boxplot <- c('#ADD8E6', '#4682B4', '#90EE90', '#3CB371')
+col_number <- c('#3cd8d8', '#d83c8a')
+
+# Define ethanol resistant OTUs and seqs! 
+otu_long <- rownames_to_column(as.data.frame(otutabEM), 'Group') %>% 
+  pivot_longer(cols = starts_with('Otu')) %>%
+  left_join(metadata %>% select(original_sample, Group, person, date), by = 'Group') %>%
+  group_by(Group) %>%
+  mutate(rel_abund = value / sum(value), 
+         PA = ifelse(value > 0, 1, 0)) %>%
+  ungroup() %>%
+  left_join(ddPCR, by = join_by('Group' == 'Sample')) %>%
+  mutate(norm_abund = rel_abund * copies) %>%
+  select(Group, name, value, original_sample, person, norm_abund, rel_abund, PA, date) %>%
+  left_join(taxtab, by = 'name')
+#saveRDS(otu_long, 'data/r_data/otu_long.RDS')
+
+# OTU that is ethanol resistant once is always ethanol resistant 
+etoh_otus <- left_join(otu_long %>% filter(substr(Group, 1, 1) == 'M'), 
+                       otu_long %>% filter(substr(Group, 1, 1) == 'S'), 
+                       by = join_by('name', 'original_sample', 'person', 'Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus')) %>%
+  # Define if OTU in a sample of stool is ethanol resistant 
+  # Contition 1: present in both bulk microbiota sample and ethanol resistant fraction
+  # Condition 2: higher relative abudnance in EtOH sample than microbiota
+  mutate(is_etoh_resistant = ifelse(value.x > 0 & value.y > 0 & rel_abund.y > rel_abund.x, 'Yes', 'No')) %>%
+  group_by(name) %>%
+  # Calculate the number of times this OTU was present in samples
+  reframe(no_present = sum(PA.x), 
+          # Caluclate how many times OTU was defined as part of EtOH fraction based on Conditions 1 & 2
+          no_Yes = ceiling(sum(is_etoh_resistant == 'Yes', na.rm = TRUE))) %>%
+  ungroup() %>%
+  # OTUs that have been defined as part Of the ethanol resistant fraction in at least 5% of samples where they were found! 
+  # (to avoid mistakes of protocol and exclude highly abundant OTUs that maybe were seen as ethanol resistant but just didn't get destoryed!)
+  filter(no_Yes > (no_present * 0.05)) %>%
+  #filter(no_Yes > (no_present * 0.1)) %>%
+  #filter(no_Yes > 1) %>%
+  # Extract names! 
+  pull(unique(name))
+#saveRDS(etoh_otus, 'data/r_data/etoh_otus.RDS')
+
+uncertain_otus <- left_join(otu_long %>% filter(substr(Group, 1, 1) == 'M'), 
+                            otu_long %>% filter(substr(Group, 1, 1) == 'S'), 
+                            by = join_by('name', 'original_sample', 'person', 'Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus')) %>%
+  mutate(is_etoh_resistant = ifelse(value.x > 0 & value.y > 0 & rel_abund.y > rel_abund.x, 'Yes', 'No')) %>%
+  group_by(name) %>%
+  reframe(no_present = sum(PA.x), 
+          no_Yes = ceiling(sum(is_etoh_resistant == 'Yes', na.rm = TRUE))) %>%
+  ungroup() %>%
+  # Filter OTUs that were detected as EtOH resistant at least once, but were detected as such in less than 5% of samples, to exclude them from the analysis 
+  filter(no_Yes > 1) %>%
+  filter(no_Yes < (no_present * 0.05)) %>%
+  pull(unique(name))
+
+nonetoh_otus <- otu_long %>% filter(substr(Group, 1, 1) == 'M' & PA == 1) %>%
+  filter(!(name %in% uncertain_otus) & !(name %in% etoh_otus)) %>%
+  pull(unique(name))
+
+# The same but for sequences 
+seq_long <- rownames_to_column(as.data.frame(seqtab), 'Group') %>% 
+  pivot_longer(cols = starts_with('VH')) %>%
+  left_join(metadata %>% select(original_sample, Group, person, date), by = 'Group') %>%
+  group_by(Group) %>%
+  mutate(rel_abund = value / sum(value), 
+         PA = ifelse(value > 0, 1, 0)) %>%
+  ungroup() %>%
+  left_join(ddPCR, by = join_by('Group' == 'Sample')) %>%
+  mutate(norm_abund = rel_abund * copies) %>%
+  select(Group, name, value, original_sample, person, norm_abund, rel_abund, PA, date) %>%
+  left_join(rownames_to_column(seq_taxtab, 'name'), by = 'name')
+
+# OTU that is ethanol resistant once is always ethanol resistant 
+etoh_seq <- left_join(seq_long %>% filter(substr(Group, 1, 1) == 'M'), 
+                      seq_long %>% filter(substr(Group, 1, 1) == 'S'), 
+                      by = join_by('name', 'original_sample', 'person', 'Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus')) %>%
+  # Define if a sequence in a sample of stool is ethanol resistant 
+  # Contition 1: present in both bulk microbiota sample and ethanol resistant fraction
+  # Condition 2: higher relative abudnance in EtOH sample than microbiota
+  mutate(is_etoh_resistant = ifelse(value.x > 0 & value.y > 0 & rel_abund.y > rel_abund.x, 'Yes', 'No')) %>%
+  group_by(name) %>%
+  # Calculate the number of times this OTU was present in samples
+  reframe(no_present = sum(PA.x), 
+          # Caluclate how many times OTU was defined as part of EtOH fraction based on Conditions 1 & 2
+          no_Yes = ceiling(sum(is_etoh_resistant == 'Yes', na.rm = TRUE))) %>%
+  ungroup() %>%
+  # OTUs that have been defined as part Of the ethanol resistant fraction in at least 10% of samples where they were found! 
+  # (to avoid mistakes of protocol and exclude highly abundant OTUs that maybe were seen as ethanol resistant but just didn't get destoryed!)
+  filter(no_Yes > (no_present * 0.05)) %>%
+  # Extract names! 
+  pull(unique(name))
+
+uncertain_seqs <- left_join(seq_long %>% filter(substr(Group, 1, 1) == 'M'), 
+                            seq_long %>% filter(substr(Group, 1, 1) == 'S'), 
+                            by = join_by('name', 'original_sample', 'person', 'Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus')) %>%
+  mutate(is_etoh_resistant = ifelse(value.x > 0 & value.y > 0 & rel_abund.y > rel_abund.x, 'Yes', 'No')) %>%
+  group_by(name) %>%
+  reframe(no_present = sum(PA.x), 
+          no_Yes = ceiling(sum(is_etoh_resistant == 'Yes', na.rm = TRUE))) %>%
+  ungroup() %>%
+  # Filter OTUs that were detected as EtOH resistant at least once, but were detected as such in less than 5% of samples, to exclude them from the analysis 
+  filter(no_Yes > 1) %>%
+  filter(no_Yes < (no_present * 0.05)) %>%
+  pull(unique(name))
+
+nonetoh_seqs <- seq_long %>% filter(substr(Group, 1, 1) == 'M' & PA == 1) %>%
+  filter(!(name %in% uncertain_seqs) & !(name %in% etoh_seq)) %>%
+  pull(unique(name))
