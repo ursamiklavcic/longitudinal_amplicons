7500a49fbaa52abdad67b488caaa79ee0e25a60e
Urša Miklavčič <ursa.miklavcic@gmail.com>
2026-02-18 08:09:58 +0000

    Modifying code and adding shotgun data to the article

---
+++ code_GutMicrobes/1_import_filter_rarefy.R
@@ -0,0 +1,178 @@
+# Analysis of V3V4 16S rRNA data from longitudinal cohort of healthy adult males - December 2023
+# OTUs were constructed in mothur, code availabile mothur.script 
+# Phylogenetic tree (sequence aligment with mafft, tree construction with FastTree), code availabile under phylogenetic_tree.bh
+
+library(dplyr)
+library(tidyr)
+library(readr)
+library(tibble)
+library(ape)
+library(vegan)
+library(stringr)
+library(lubridate)
+library(readxl)
+library(ggplot2)
+
+set.seed(96)
+theme_set(theme_bw(base_size = 14))
+
+# Exploration
+shared = read_tsv('data/mothur/final.opti_mcc.shared') %>%
+  select(Group, starts_with('Otu')) %>%
+  pivot_longer(-Group)
+
+# Min number of reads in sample and min sequences per OTU as determined in the exploration analysis
+reads_per_sample = 150000 # we loose 6 samples
+min_seqs_per_otu = sum(shared$value)*0.000001 # remove 0.0001% which is 0.000001
+
+shared_pre = shared %>%
+  group_by(Group) %>%
+  # Count the number of reads in each sample
+  mutate(sum_sample = sum(value)) %>%
+  # and remove all from microbiota and sporobiota, that have less than 100 000 
+  filter(sum_sample > reads_per_sample) %>%
+  ungroup() %>%
+  group_by(name) %>%
+  # Count the number of reads each OTU has
+  mutate(sum_otus = sum(value)) %>%
+  # Remove OTUs that have less than 0,000001% reads in total
+  filter(sum_otus > min_seqs_per_otu) %>%
+  ungroup() %>%
+  select(-sum_otus, -sum_sample)
+
+# Data for EtOH-EMA fraction VS microbiota samples 
+otutabEM_pre = shared_pre %>%
+  pivot_wider(names_from = 'name', values_from = 'value', values_fill = 0) %>%
+  column_to_rownames('Group')
+
+# Rarefy the data once - as we sequenced so deep that for the first analysis this is not crucial !
+otutabEM = rrarefy(otutabEM_pre, sample=reads_per_sample)
+saveRDS(otutabEM, 'data/r_data/otutabEM.RDS')
+
+# Extract OTUs that are present rarefied table 
+otu_names = as.data.frame(otutabEM) %>% colnames() 
+
+# Import taxonomy table
+taxtab <- read_tsv('data/mothur/final.opti_mcc.0.03.cons.taxonomy') %>%
+  filter(OTU %in% otu_names) %>%
+  select(name = "OTU", taxonomy = "Taxonomy") %>%
+  mutate(taxonomy = str_replace_all(taxonomy, "\\\\|\\\"|\\(\\d+\\)", ""),
+         taxonomy = str_replace(taxonomy, ";$", "")) %>%
+  separate(taxonomy, into=c("Domain", "Phylum", "Class", "Order", "Family", "Genus"),
+           sep=";") %>% 
+  mutate(Phylum = case_when(
+    Phylum == 'Firmicutes' ~ 'Bacillota',
+    Phylum == 'Bacteroidetes' ~ 'Bacteroidota',
+    Phylum == 'Actinobacteria' ~ 'Actinomycetota',
+    Phylum == 'Proteobacteria' ~ 'Pseudomonadota',
+    Phylum == 'Bacteria_unclassified' ~ 'unclassified Bacteria',
+    Phylum == 'Verrucomicrobia' ~ 'Verrucomicrobiota',
+    Phylum == 'Tenericutes' ~ 'Mycoplasmatota',
+    Phylum == 'Fusobacteria' ~ 'Fusobacteriota',
+    Phylum == 'Lentisphaerae' ~ 'Lentisphaerota',
+    Phylum == 'Synergistetes' ~ 'Synergistota',
+    Phylum == 'TM7' ~ 'Saccharibacteria',
+    Phylum == 'Deferribacteres' ~ 'Deferribacterota',
+    TRUE ~ Phylum )) 
+
+saveRDS(taxtab, 'data/r_data/taxtab.RDS')
+
+# Import metadata
+metadata = as_tibble(read.csv('data/metadata.csv', sep=';')) %>%
+  mutate(date=dmy(date)) %>%
+  filter(Group %in% rownames(otutabEM)) %>%
+  mutate(biota = ifelse(biota == 'bulk microbiota', 'untreated sample', 'ethanol treated sample'))
+saveRDS(metadata, 'data/r_data/metadata.RDS')
+
+# # Phylogenetic analysis 
+# # Load tree file from mafft 
+# tree_pre = ape::read.tree('data/phylo_analysis/fasttree_mafft_align.tree')
+# 
+# # Final count table (before making OTUs)
+# counttab = read.table('data/phylo_analysis/final.full.count_table', header = TRUE, sep = "\t") 
+# 
+# # Min number of reads in sample and min sequences per OTU as determined in the exploration analysis
+# min_seqs = sum(counttab$total)*0.000001
+# 
+# # Statistics
+# counttab_stat = counttab %>% 
+#   filter(total > min_seqs) %>%
+#   pivot_longer(names_to = 'Group', values_to = 'value', cols = c(starts_with("S"), starts_with("M")) ) %>%
+#   # remove negative controls
+#   filter(Group != 'SNC') %>%
+#   filter(Group != 'MNC') %>%
+#   group_by(Group) %>%
+#   summarize(sum = sum(value))
+# 
+# # Number of reads per sample 
+# max(counttab_stat$sum) #880447
+# min(counttab_stat$sum) #59870
+# mean(counttab_stat$sum) #264166.3
+# median(counttab_stat$sum) #230052.5
+# sum(counttab_stat$sum)# 61286585
+
+# # Stat on sequences
+# counttab_stat_seq = counttab %>% 
+#   pivot_longer(names_to = 'Group', values_to = 'value', cols = c(starts_with("S"), starts_with("M"))) %>%
+#   filter(total > min_seqs)
+# 
+# min(counttab_stat_seq$total) #63
+# max(counttab_stat_seq$total) #13963206
+# mean(counttab_stat_seq$total) #17416.4
+# median(counttab_stat_seq$total) #208
+# 
+# # Get the name of samples that have less than 150 000 reads per sample!
+# # Remove sequneces with less than 10 reads all togther !
+# # filter counttab 
+# seqtab_pre = counttab %>% 
+#   # remove sequences with less than 10 reads
+#   filter(total > min_seqs) %>%
+#   pivot_longer(names_to = 'Group', values_to = 'value', cols = c(starts_with("S"), starts_with("M")) ) %>%
+#   group_by(Group) %>%
+#   # Count the number of reads in each sample
+#   mutate(sum_sample = sum(value)) %>%
+#   # and remove all from microbiota and sporobiota, that have less than 100 000 
+#   filter(sum_sample > reads_per_sample) %>%
+#   ungroup() %>%
+#   select(-sum_sample)
+# 
+# # make sequence table (like otutab)
+# seqtab_pre1 = select(seqtab_pre, Representative_Sequence, Group, value) %>% 
+#   pivot_wider(names_from = 'Group', values_from = 'value') %>%
+#   column_to_rownames('Representative_Sequence')
+# 
+# # rarefy the table to 100 000 reads per sample
+# seqtab =  rrarefy(t(seqtab_pre1), sample=reads_per_sample) %>% as.data.frame()
+# 
+# # Taxonomy 
+# seq_taxtab = read.csv('data/phylo_analysis/final.taxonomy', header=FALSE, sep='\t') %>%
+#   rename(seq = V1, taxa = V2) %>%
+#   mutate(taxa = str_replace_all(taxa, "\\\\|\\\"|\\(\\d+\\)", ""),
+#          taxa = str_replace(taxa, ";$", "")) %>%
+#   separate(taxa, into=c("Domain", "Phylum", "Class", "Order", "Family", "Genus"),
+#            sep=";") %>%
+#   filter(seq %in% colnames(seqtab)) %>%
+#   column_to_rownames('seq')
+# 
+# # Make sure that sequences that we filtered are removed from the tree as well 
+# tree = ape::drop.tip(phy=tree_pre, 
+#                      tip=setdiff(tree_pre$tip.label, colnames(seqtab)))
+
+
+saveRDS(seqtab, 'data/r_data/seqtab.RDS')
+saveRDS(seq_taxtab, 'data/r_data/seq_taxtab.RDS')
+saveRDS(tree, 'data/r_data/tree.RDS')
+
+# remove unnecessary 
+rm(otutab_pre)
+rm(otutabEM_pre)
+rm(seqtab_pre)
+rm(seqtab_pre1)
+rm(tree_pre)
+rm(counttab)
+rm(counttab_stat_seq)
+rm(counttab_stat)
+rm(min_seqs)
+rm(min_seqs_per_otu)
+rm(otu_names)
+rm(reads_per_sample)
---
+++ code_GutMicrobes/2_absolute_quantification.R
@@ -0,0 +1,103 @@
+# Library 
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(vegan)
+library(tibble)
+library(readxl)
+library(scales)
+# Code for impelemnting absolute quantification of thw 16S rRNA gene measured by ddPCR 
+# DNA for microbiota samples was diluted to 5 ng/ul * 10E-6
+# primers used were the same as for the 16S rRNA gene sequencinf v3-v4 region 
+
+# Barlow et al., 2020 & Manzari et al., 2020
+
+# function to read the QuantaSoft file, as it is just a mess
+read.quantasoft = function(file) {
+  # Read the lines
+  lines = readLines(file)
+  # Extract header 
+  header = strsplit(lines[1], ',')[[1]]
+  # Spilt by , 
+  data_lines = sapply(lines[-1], function(line) strsplit(substr(line, 2, nchar(line)), ",")[[1]])
+  # turn into data.frame and add header and remove row.names
+  data_df = as.data.frame(t(data_lines), stringsAsFactors = FALSE)
+  names(data_df) = header
+  rownames(data_df) = NULL
+  # Select only the info I need, remove " from names of well and samples
+  data_df_fin = data_df %>% select(Well, Sample, Concentration, CopiesPer20uLWell, Positives, Negatives, AcceptedDroplets) %>%
+    filter(Concentration != 'No Call') %>%
+    transform(Concentration = as.numeric(Concentration), 
+              CopiesPer20uLWell = as.numeric(CopiesPer20uLWell), 
+              Positives = as.numeric(Positives),
+              Negatives = as.numeric(Negatives),
+              AcceptedDroplets = as.numeric(AcceptedDroplets)) %>%
+    mutate(across(c(Well, Sample), ~gsub('"', '', .)))
+
+}
+
+# 
+samples_info = read_excel('data/vzorci.xlsx', sheet = 6)
+
+plate1_m  = read.quantasoft('data/absolute_quantification/original_files/20240322_ddPCR_v3v4_microbiota_plate1_results.csv') %>%
+  filter(AcceptedDroplets > 10000 & Positives > 12)
+
+plate2_m = read.quantasoft('data/absolute_quantification/original_files/20240322_ddPCR_v3v4_microbiota_plate2_results.csv') %>%
+  filter(AcceptedDroplets > 10000 & Positives > 8)
+
+plates_e = read.quantasoft('data/absolute_quantification/original_files/20240513_ddPCR_v3v4_sporobiota_1_results.csv') %>%
+  rbind(read.quantasoft('data/absolute_quantification/original_files/20240513_ddPCR_v3v4_sporobiota_2_results.csv')) %>%
+  filter(AcceptedDroplets > 10000 & Positives > 1) %>%
+  # Exclude from analysis because the amount of DNA was insufficient SB008, SB009, SE002, SE003, SF001, SF009, SH007, SC013
+  filter(!(Sample %in% c('SB008', 'SB009', 'SE002', 'SE003', 'SF001', 'SF009', 'SH007', 'SC013')))
+
+sample = read.quantasoft('data/absolute_quantification/original_files/MA001.csv') 
+
+ddPCR = rbind(plate1_m, plate2_m, plates_e, sample) %>%
+  group_by(Sample) %>%
+  summarise(Concentration = mean(Concentration)) %>%
+  left_join(samples_info, by =join_by('Sample'=='Group')) %>%
+  # Calculate the copy number of 16s rRNA gene per ng of DNA
+  # concentration = Poisson correlted value copies per ul
+  # 25/2.5 = adjust for the amount of DNA in reaction
+  # 25/20 = adjust for the reaction made VS reaction used
+  # dilution of the DNA 
+  # dilution from original samples to normalized value
+  mutate(copies = (Concentration * (25/2.5) * (25/20) * dilution_ddPCR * (DNAconc/DNAconc_seq)))
+
+saveRDS(ddPCR, 'data/r_data/ddPCR.RDS')
+
+# Additional experiment: Does PowerFecal kit isolate spores?
+samples = read_excel('data/absolute_quantification/PowerFecal/samples.xlsx')
+
+pf = read.quantasoft('data/absolute_quantification/PowerFecal/ddpcr_cdiff_20240808_RESULTS.csv') %>%
+  right_join(samples, by = 'Sample') %>%
+  mutate(abs_conc = Concentration * (25/2.5) * (25/20) * 1000) 
+
+pf$sample_description = factor(pf$sample_description, levels = c('negative control (H2O)', 'negative control (E. coli)','fecal sample', 'fecal sample + 10e-4 spores', 'fecal sample + 10e-3 spores', 
+                                                        'fecal sample + 10e-1 spores', 'fecal sample with undiluted spores','positive control (C.diff)'))
+
+# Present only the ones important for correlation! 
+pf_cor <- filter(pf, sample_description %in% c('fecal sample + 10e-4 spores', 'fecal sample + 10e-3 spores', 
+                                           'fecal sample + 10e-1 spores', 'fecal sample with undiluted spores')) %>% 
+  mutate(sample_description = substr(sample_description, 16, 37), 
+         sample_description = str_remove(sample_description, " spores"), 
+         sample_description = ifelse(sample_description == 'th undiluted', 'undiluted', sample_description))
+pf_cor$sample_description = factor(pf_cor$sample_description, levels = c('undiluted', '10e-1', '10e-3', '10e-4'))
+cor_pf <- cor.test(pf_cor$Concentration, pf_cor$sporeCFU, method = 'pearson')
+
+pf_cor %>%
+  ggplot(aes(x = sporeCFU, y = abs_conc, color = sample_description )) +
+  geom_point(size = 4) +
+  geom_abline() +
+  annotate('text', x= 1e7, y = 1e9, 
+           label = paste("Pearson's correlation:", round(cor_pf$estimate, digits =2), '\n', 
+                         'p-value =', round(cor_pf$p.value, digits = 3))) +
+  scale_x_continuous(trans = log_trans(), breaks = c(0, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11)) +
+  scale_y_continuous(trans = log_trans(), breaks = c(0, 1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10)) +
+  labs(x = expression(italic("C. difficile") * " spores CFU/ml"), 
+       y = expression("Copies of " * italic("C. difficile") * " 16S rRNA/ml"), color = 'Dilution factor')
+ggsave('out/exploration/PowerFecal_spores.png', dpi = 600)
+ggsave('out/figures/PowerFecal_spores.png', dpi=600)
+
+
